ab7b1e60e7256f022609dfda4e9ce5a75eebf9a9
Cid <caiodiasfleury@gmail.com>
2025-03-03 19:00:31 -0300

    blusa adicionada

--- src/scripts/3dloader.js
+++ src/scripts/3dloader.js
@@ -15,41 +15,29 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 );
-
 const loader = new GLTFLoader();
 
 //Load the file
-loader.load(
-  `../modelos/scene.gltf`,
-  function (gltf) {
-    //If the file is loaded, add it to the scene
-    let object = gltf.scene;
-    scene.add(object);
-  },
-  function (xhr) {
-    //While it is loading, log the progress
-    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-  },
-  function (error) {
-    //If there is an error, log it
-    console.error(error);
-  }
-);
+loader.load('../modelos/t_shirt_hoodie_3d_model/scene.gltf',(gltfScene)=>{
+  scene.add(gltfScene.scene);
+});
+  
 
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
 topLight.position.set(500, 500, 500) //top-left-ish
 topLight.castShadow = true;
 scene.add(topLight);
 
-const axesHelper = new THREE.AxesHelper(5);
-
-scene.add(axesHelper);
-
-camera.position.set(0,2,5);
+camera.position.set(0,0,7);
 
 const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
 const box = new THREE.Mesh(boxGeometry,boxMaterial);
-scene.add(box);
+//scene.add(box);
 
-renderer.render(scene,camera);
+function animate(time) {
+  scene.rotation.y = time/2000;
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
