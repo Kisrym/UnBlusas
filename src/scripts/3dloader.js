b3c1ba32dd78dcfd7df7891f0e4ce88596634d4c
Kaio <kaiosantos568@gmail.com>
2025-03-03 16:47:17 -0300

    Melhora na home e início do 3dloader

---
+++ src/scripts/3dloader.js
@@ -0,0 +1,55 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+
+const renderer = new THREE.WebGLRenderer({alpha:true});
+
+renderer.setSize(window.innerWidth,window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
+
+const loader = new GLTFLoader();
+
+//Load the file
+loader.load(
+  `../modelos/scene.gltf`,
+  function (gltf) {
+    //If the file is loaded, add it to the scene
+    let object = gltf.scene;
+    scene.add(object);
+  },
+  function (xhr) {
+    //While it is loading, log the progress
+    console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  function (error) {
+    //If there is an error, log it
+    console.error(error);
+  }
+);
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500) //top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
+
+const axesHelper = new THREE.AxesHelper(5);
+
+scene.add(axesHelper);
+
+camera.position.set(0,2,5);
+
+const boxGeometry = new THREE.BoxGeometry();
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00FF00});
+const box = new THREE.Mesh(boxGeometry,boxMaterial);
+scene.add(box);
+
+renderer.render(scene,camera);
