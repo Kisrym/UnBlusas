d9f0be5ae60de22525d5b8774d82111e56b1e11e
Cid <caiodiasfleury@gmail.com>
2025-03-05 12:31:33 -0300

    a2

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -3,5 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch("../components/header.html").then(response => response.text()).then(data => {
             document.getElementById("header-container").innerHTML = data;
         });
+    }else
+    {
+        fetch("src/components/header.html").then(response => response.text()).then(data => {
+            document.getElementById("header-container").innerHTML = data;
+        });
     }
 });
