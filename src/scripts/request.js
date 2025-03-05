a2f7a31965b3bfa94a4cd1b9f68be058fb39afd7
Kaio <kaiosantos568@gmail.com>
2025-03-05 11:11:16 -0300

    Server side e requests

---
+++ src/scripts/request.js
@@ -0,0 +1,27 @@
+
+document.getElementById('botaoPedido').addEventListener('click', () => {
+    // temporario
+    const prompt = 'A beautiful sunset over the city of São Paulo, Brazil.';
+    const size = '1024x1024';
+    //
+
+    fetch('http://localhost:5000/generateimage', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }, 
+        body: JSON.stringify({ prompt, size }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.image) {
+                console.log(`Imagem gerada:\n${data.image}`);
+            }
+            else {
+                console.log(`Erro ao gerar imagem: ${data.error}`);
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao fazer requisição para a API:', error);
+        });
+});
