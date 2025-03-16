33264d2cef6a27da93e72803deb009d8568a1ae9
Cid <caiodiasfleury@gmail.com>
2025-03-16 20:04:14 -0300

    okok

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -5,12 +5,12 @@ document.getElementById("cadastro-form").addEventListener("submit", async functi
     const email = document.getElementById("email").value;
     const senha = document.getElementById("senha").value;
 
-    const response = await fetch("http://localhost:3000/cadastrar", {
+    const response = await fetch("https://unblusas-api-production.up.railway.app/cadastrar", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ nome, email, senha })
     });
 
     const result = await response.json();
-    document.getElementById("mensagem").innerText = result.mensagem;
+    document.getElementById("mensagem").innerText = result.message;
 });
