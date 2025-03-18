0e313f97f3ae6737da0d3282f047d87e10edd53e
Cid <caiodiasfleury@gmail.com>
2025-03-17 23:34:29 -0300

    Login Update

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,3 +1,19 @@
+document.getElementById("login-form").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("lemail").value;
+    const senha = document.getElementById("lsenha").value;
+
+    const response = await fetch("https://unblusas-api-production.up.railway.app/login", { // Rota corrigida
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, senha }) // Chaves corrigidas
+    });
+
+    const result = await response.json();
+    document.getElementById("lmensagem").innerText = result.message;
+});
+
 document.getElementById("cadastro-form").addEventListener("submit", async function(event) {
     event.preventDefault();
 
